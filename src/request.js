aacd0fa8e97a03978fe6770ef25a8de65edaf79a
jackieLin <dashi_lin@163.com>
2016-03-28 19:32:09 +0800

    update: 重新请求

--- src/request.js
+++ src/request.js
@@ -23,7 +23,19 @@ module.exports = (function(window){
             // IE 6 and older
             this.httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
         }
-        
+
+        this.watch();
+    };
+    
+    /**
+     * 监听事件
+     */
+    Request.prototype.watch = function() {
+        var self = this;
+        this.httpRequest.onerror = function() {
+            self.getScript(self._url);
+        };
+
         this.httpRequest.onreadystatechange = (function(context) {
             return function() {
                 if(context.httpRequest.readyState === 4) {
@@ -36,19 +48,46 @@ module.exports = (function(window){
                 }        
             };
         })(this);
-
-        this.httpRequest.onerror = function() {
-            
-        };
     };
-    
+
+    /**
+     * script 标签写入
+     */
+    Request.prototype.getScript = function(url) {
+        var script = document.createElement('script');
+        script.src = url;
+        document.head.appendChild(script);
+        this._success.apply(this, []);
+    };
+
+
     /**
      * url 是否跨域
+     * @see https://github.com/jquery/jquery/blob/93a8fa6bfc1c8a469e188630b61e736dfb69e128/src/ajax.js#L535
      */
-    Request.prototype.crossDomain = function(url) {
+    Request.prototype.isCrossDomain = function(url) {
+        crossDomain = null;
+
         if(!url) {
             return false;
         }
+
+        var urlAnchor = document.createElement( "a" );
+
+        try {
+            urlAnchor.href = url;
+
+            // Support: IE8-11+
+            // Anchor's host property isn't correctly set when s.url is relative
+            urlAnchor.href = urlAnchor.href;
+        } catch ( e ) {
+
+            // If there is an error parsing the URL, assume it is crossDomain,
+            // it can be rejected by the transport if it is invalid
+            crossDomain = true;
+        }
+
+        return crossDomain;
     };
 
     Request.prototype.setRequestHeader = function() {
@@ -68,6 +107,7 @@ module.exports = (function(window){
             this._success = callback;
         }
         
+        this._url = url;
         this.httpRequest.open('GET', url, true);
         this.httpRequest.send(null);
     };
